8f839e5c3d600a10d3a83798aefd676d41aac94d
AniketRabade <rabadeaniket1@gmail.com>
2025-10-17 11:43:38 +0530

    dotenv added

--- Brfle/client/src/store/api/authAPI.js
+++ Brfle/client/src/store/api/authAPI.js
@@ -1,47 +1,50 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth'; 
+// const API_URL = 'http://localhost:5000/api/auth'; 
+// import propelry api url from .env fil
+const API_URL = import.meta.env.VITE_API_URL;
+console.log("API_URL:", API_URL);
 
 const authAPI = {
   // Register user
   register: async (userData) => {
-    const response = await axios.post(`${API_URL}/register`, userData);
+    const response = await axios.post(`${API_URL}/auth/register`, userData);
     return response;
   },
 
   // Login user
   login: async (credentials) => {
-    const response = await axios.post(`${API_URL}/login`, credentials);
+    const response = await axios.post(`${API_URL}/auth/login`, credentials);
     return response;
   },
 
   // Logout user
   logout: async () => {
-    const response = await axios.post(`${API_URL}/logout`);
+    const response = await axios.post(`${API_URL}/auth/logout`);
     return response;
   },
 
   // Get current user
   getMe: async () => {
-    const response = await axios.get(`${API_URL}/me`);
+    const response = await axios.get(`${API_URL}/auth/me`);
     return response;
   },
 
   // Update user profile
   updateProfile: async (userData) => {
-    const response = await axios.put(`${API_URL}/myprofile`, userData);
+    const response = await axios.put(`${API_URL}/auth/myprofile`, userData);
     return response;
   },
 
   // Forgot password
   forgotPassword: async (email) => {
-    const response = await axios.post(`${API_URL}/forgot-password`, { email });
+    const response = await axios.post(`${API_URL}/auth/forgot-password`, { email });
     return response;
   },
 
   // Reset password
   resetPassword: async (token, passwords) => {
-    const response = await axios.put(`${API_URL}/reset-password/${token}`, passwords);
+    const response = await axios.put(`${API_URL}/auth/reset-password/${token}`, passwords);
     return response;
   },
 };
--- Brfle/client/src/store/api/paymentApi.js
+++ Brfle/client/src/store/api/paymentApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/payments';
+// const API_URL = 'http://localhost:5000/api/payments';
+const API_URL = `${import.meta.env.VITE_API_URL}/payments`;
 
 const api = axios.create({
   baseURL: API_URL,
